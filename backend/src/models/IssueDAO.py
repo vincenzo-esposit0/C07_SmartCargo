9c8325a0ce6ab97046a482701734f1232a0e632a
Roksid2002 <roksid09@gmail.com>
2024-01-02 17:09:30 +0100

    aggiunta issueDAO

---
+++ backend/src/models/IssueDAO.py
@@ -0,0 +1,38 @@
+from sqlalchemy.orm import sessionmaker
+from backend.src.models import Issue
+from backend.src.config.database import engine, Session
+
+class IssueDAO:
+    def __init__(self):
+        self.Session = sessionmaker(bind=engine)
+
+    def aggiungi_issue(self, issue):
+        session = self.Session()
+        session.add(issue)
+        session.commit()
+        session.close()
+
+    def ottieni_tutte_issues(self):
+        session = self.Session()
+        issues = session.query(Issue.Issue).all()
+        session.close()
+        return issues
+
+    def ottieni_issue_per_id(self, issue_id):
+        session = self.Session()
+        issue = session.query(Issue.Issue).filter_by(id=issue_id).first()
+        session.close()
+        return issue
+
+    def aggiorna_issue(self, issue):
+        session = self.Session()
+        session.merge(issue)
+        session.commit()
+        session.close()
+
+    def elimina_issue(self, issue_id):
+        session = self.Session()
+        issue = session.query(Issue.Issue).filter_by(id=issue_id).first()
+        session.delete(issue)
+        session.commit()
+        session.close()
