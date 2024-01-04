1324fb7e7c2ce48a90ba76fa11b3ca299321e6aa
PaoloMurino <p.murino1@studenti.unisa.it>
2024-01-04 18:15:44 +0100

    creazione VeicoloController.py

--- backend/VeicoloController.py
+++ backend/VeicoloController.py
@@ -1,41 +1,34 @@
-from src.models.Veicolo import Veicolo
 from src.models.VeicoloDAO import VeicoloDAO
 
-# Creazione di un'istanza di VeicoloDAO
 veicolo_dao = VeicoloDAO()
 
-# Ottieni tutti i veicoli
-print("Tutti i veicoli:")
-tutti_veicoli = veicolo_dao.ottieni_tutti_veicoli()
-for veicolo in tutti_veicoli:
-    #L'uso della f indica che si sta formattando una stringa
-    print(f"ID: {veicolo.id}, Targa: {veicolo.targa}, Modello: {veicolo.modello}, Descrizione: {veicolo.descrizione}")
+def ottieniTuttiVeicoli():
+    try:
+        veicoli = veicolo_dao.ottieni_tutti_veicoli()
 
-# Ottieni un veicolo con ID 3
-print("\nVeicolo con ID 3:")
-veicolo_id = 3
-veicolo_3 = veicolo_dao.ottieni_veicolo_per_id(veicolo_id)
-if veicolo_3:
-    print(f"ID: {veicolo_3.id}, Targa: {veicolo_3.targa}, Modello: {veicolo_3.modello}, Descrizione: {veicolo_3.descrizione}")
-else:
-    print(f"Nessun veicolo trovato con ID {veicolo_id}")
+        if veicoli:
+            # Utilizza una lista per ottenere una lista di JSON
+            veicoliJson = [veicolo.__json__() for veicolo in veicoli]
 
-# Inserisci un nuovo veicolo
-nuovo_veicolo = Veicolo(targa='XYZ123', modello='Nuovo Modello', descrizione='Descrizione del nuovo veicolo')
-veicolo_dao.aggiungi_veicolo(nuovo_veicolo)
+            # Restituisce la lista di JSON come risultato
+            return veicoliJson
+        else:
+            return {"message": "Veicoli non trovati"}
 
-# Ottieni tutti i veicoli dopo l'inserimento
-print("\nTutti i veicoli dopo l'inserimento:")
-tutti_veicoli_dopo = veicolo_dao.ottieni_tutti_veicoli()
-for veicolo in tutti_veicoli_dopo:
-    print(f"ID: {veicolo.id}, Targa: {veicolo.targa}, Modello: {veicolo.modello}, Descrizione: {veicolo.descrizione}")
+    except Exception as e:
+        print(f"Errore durante l'ottenimento dei veicoli: {str(e)}")
+        return {}
 
-# Elimina un veicolo (ad esempio, con ID 2)
-veicolo_da_eliminare_id = 21
-veicolo_dao.elimina_veicolo(veicolo_da_eliminare_id)
+def ottieniVeicolo(veicolo_id):
+    try:
+        veicolo = veicolo_dao.ottieni_veicolo_per_id(veicolo_id)
 
-# Ottieni tutti i veicoli dopo l'eliminazione
-print("\nTutti i veicoli dopo l'eliminazione:")
-tutti_veicoli_dopo_elim = veicolo_dao.ottieni_tutti_veicoli()
-for veicolo in tutti_veicoli_dopo_elim:
-    print(f"ID: {veicolo.id}, Targa: {veicolo.targa}, Modello: {veicolo.modello}, Descrizione: {veicolo.descrizione}")
+        if veicolo:
+            # Restituisci i dettagli del veicolo come JSON
+            return veicolo.__json__()
+        else:
+            return {"message": "Veicolo non trovato"}
+
+    except Exception as e:
+        print(f"Errore durante l'ottenimento del veicolo: {str(e)}")
+        return {}
