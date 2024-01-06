da5ff1cab2bb84007180fa3ea3ef5f619324f978
PaoloMurino <p.murino1@studenti.unisa.it>
2024-01-06 13:04:51 +0100

    Correzione test.py

--- backend/src/dataManagement/ingresso/IngressoController.py
+++ backend/src/dataManagement/ingresso/IngressoController.py
@@ -17,17 +17,24 @@ include_dao = IncludeDAO()
 
 def registrazioneIngresso(ingressoJson):
     try:
+        operazioneJson = ingressoJson["operazione"]
+        tipoOperazioneJson = operazioneJson["tipo"]
+        destinazioneOperazioneJson = ingressoJson["destinazione"]
+
         autotrasportatoreJson = ingressoJson["autotrasportatore"]
         autotrasportatore = autotrasportatore_dao.get_autotrasportatore_per_ingresso(autotrasportatore_nome=autotrasportatoreJson["nome"],
                                                                                      autotrasportatore_cognome=autotrasportatoreJson["cognome"],
                                                                                      autotrasportatore_azienda=autotrasportatoreJson["azienda"])
 
         merceJson = ingressoJson["merci"]
+        tipoMerceJson = merceJson["tipo"]
 
         veicoloJson = ingressoJson["veicolo"]
-
+        modelloJson = veicoloJson["modello"]
+        """
         veicolo = veicolo_dao.get_veicolo_per_ingresso(veicolo_modello=veicoloJson["modello"],
                                                        veicolo_targa=veicoloJson["targa"])
+        """
 
         #Recupero tutti gli operatori di magazzino disponibili
         operatoriMagazzino_disponibili = operatoreMagazzino_dao.ottieni_tutti_operatori_magazzino()
@@ -36,20 +43,21 @@ def registrazioneIngresso(ingressoJson):
         operatoreMagazzinoScelto = random.choice(operatoriMagazzino_disponibili)
 
         operazione = Operazione(
-            tipo=ingressoJson["tipo"],
-            puntoDestinazione=ingressoJson["destinazione"],
+            tipo=tipoOperazioneJson["nome"],
+            puntoDestinazione=destinazioneOperazioneJson["nome"],
             stato="In Corso",
+            descrizione=operazioneJson["descrizione"],
             autotrasportatore_id=autotrasportatore.id,
             operatoreIngresso_id=ingressoJson["operatoreIngresso_id"],
             operatoreMagazzino_id=operatoreMagazzinoScelto.id,
             percorso_id=1, #da rivedere
-            veicolo_id=veicolo.id
+            veicolo_id=modelloJson["id"]
         )
         result = operazione_dao.aggiungi_operazione(operazione)
 
         include = Include(
             operazione_id=operazione.id,
-            merce_id=merceJson["merce_id"],
+            merce_id=tipoMerceJson["id"],
             quantita=merceJson["quantita"]
         )
         include_dao.aggiungi_include(include)
