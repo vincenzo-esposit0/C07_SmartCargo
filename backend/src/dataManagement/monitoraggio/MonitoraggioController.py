fcb2d1f9c67c204f69f4ac752101ec1a63b4c2fb
Roksid2002 <roksid09@gmail.com>
2024-01-09 15:22:52 +0100

    aggiunta metodo visualzzazione merci op di magazzino

--- backend/src/dataManagement/monitoraggio/MonitoraggioController.py
+++ backend/src/dataManagement/monitoraggio/MonitoraggioController.py
@@ -1,5 +1,5 @@
-from backend.src.dataManagement.service import OperazioneService
-from backend.src.models.AutotrasportatoreDAO import AutotrasportatoreDAO
+from src.dataManagement.service import OperazioneService, AutotrasportatoreService, IncludeService, MerceService
+from src.models.AutotrasportatoreDAO import AutotrasportatoreDAO
 from backend.src.models.OperazioneDAO import OperazioneDAO
 
 autotrasportatore_dao = AutotrasportatoreDAO()
@@ -28,3 +28,35 @@ def storicoOperazioniPerIssue(storicoJson):
         return {}
 
 
+def ottieni_tutte_Operazioni_e_Merci():
+    try:
+        operazioni = operazione_dao.ottieni_operazioni_per_stato("In corso / Regolare")
+
+        if operazioni:
+            operazioniJson = []
+            for operazione in operazioni:
+
+                # Ottieni le informazioni dell'autotrasportatore usando l'ID
+                autotrasportatore = AutotrasportatoreService.ottieniAutotrasportatorePerId(operazione.autotrasportatore_id)
+
+                include = IncludeService.ottieniIncludePerIdOperazione(operazione.id)
+
+                # Ottieni le informazioni della merce usando l'ID
+                merce = MerceService.ottieniMercePerId(include["merce_id"])
+
+                operazioneJson = {
+                    "operazione": operazione.__json__(),
+                    "autotrasportatore": autotrasportatore,
+                    "merce":  merce,
+                }
+
+                operazioniJson.append(operazioneJson)
+
+            # Restituisce la lista di JSON come risultato
+            return operazioniJson
+        else:
+            return {"message": "Operazioni non trovate"}
+
+    except Exception as e:
+        print(f"Errore durante l'ottenimento delle operazioni: {str(e)}")
+        return {}
