4ae9c98a46f6c1af9392ac733367a9da004448ba
andree333 <a.crinteaoprisescu@studenti.unisa.it>
2024-01-19 16:31:29 +0100

    modifica per category partition

--- backend/src/unitTests/RegistrazioneAutotrasportatoreTest.py
+++ backend/src/unitTests/RegistrazioneAutotrasportatoreTest.py
@@ -1,21 +1,17 @@
 import unittest
-from datetime import datetime, date
-from unittest.mock import patch, MagicMock
+
+from unittest.mock import MagicMock
 from backend.src.dataManagement.account import AccountAutotrasportatoreController
-from backend.src.dataManagement.account.AccountAutotrasportatoreController import modificaAutotrasportatore
-from backend.src.dataManagement.issue import IssueController
-from backend.src.models.UtenteRegistrato import Autotrasportatore
 
 
 class TestAccountAutotrasportatoreController(unittest.TestCase):
 
     def setUp(self):
-            # Configurare oggetti o variabili comuni per tutti i test
-            self.mock_autotrasportatore_dao = MagicMock()
+             self.mock_autotrasportatore_dao = MagicMock()
 
-
+    #Caso input corretto
     def test_modificaAutotrasportatore(self):
-            # Creare un oggetto JSON di esempio per il test
+
             self.mock_autotrasportatore_dao.__json__ = MagicMock(return_value = {
                 "id": 1,
                 "nome": "NomeTest",
@@ -42,9 +38,6 @@ class TestAccountAutotrasportatoreController(unittest.TestCase):
                     "azienda": "AziendaTest"
             }
 
-            self.mock_autotrasportatore_dao.ottieni_autotrasportatore_per_id.return_value = self.mock_autotrasportatore_dao
-            self.mock_autotrasportatore_dao.aggiorna_autotrasportatore.return_value = self.mock_autotrasportatore_dao
-
 
             # Chiamare la funzione da testare
             response = AccountAutotrasportatoreController.modificaAutotrasportatore(autotrasportatoremod)
@@ -56,19 +49,8 @@ class TestAccountAutotrasportatoreController(unittest.TestCase):
             # Verificare che la funzione restituisca il risultato atteso
             self.assertEqual(response, self.mock_autotrasportatore_dao.__json__.return_value)
 
-        #@patch('backend.src.models.AutotrasportatoreDAO')
-    """def test_modificaAutotrasportatore_exception(self):
 
-            # Simula un'eccezione nel dao
-            self.mock_autotrasportatore_dao.ottieni_autotrasportatore_per_id.side_effect = Exception("Errore simulato")
-
-            # Chiamata al metodo di modifica con eccezione attesa
-            result = AccountAutotrasportatoreController.modificaAutotrasportatore({})
-
-            # Verifica che il risultato contenga il messaggio di errore
-            expected_result = {"message": "Errore durante la modifica dell'account di un autotrasportatore"}
-            self.assertEqual(result, expected_result)"""
-
+    #Caso input vuoto
     def test_modificaAutotrasportatore_not_found(self):
 
                 # Simula un'eccezione nel dao
@@ -81,18 +63,54 @@ class TestAccountAutotrasportatoreController(unittest.TestCase):
                 expected_result = {"message": "Errore durante la modifica dell'account di un autotrasportatore"}
                 self.assertEqual(result, expected_result)
 
+    #campo input chiave vuota
+    def test_modificaAutotrasportatore_valoreChiaveVuoto(self):
+        self.mock_autotrasportatore_dao.__json__ = MagicMock(return_value = {
+            "id": 1,
+            "nome": "NomeTest",
+            "cognome": "",
+            "dataNascita": "1990-01-01",
+            "codiceFiscale": "RSSMRA85M01H501Z",
+            "email": "test@example.com",
+            "password": "testpassword",
+            "indirizzo": "Via Test, 123",
+            "azienda": "AziendaTest",
+            "qrCode_id": 1
+        })
+
+        result = AccountAutotrasportatoreController.modificaAutotrasportatore(self.mock_autotrasportatore_dao.__json__.return_value)
+
+        # Verificare che la funzione restituisca il risultato atteso
+        self.assertEqual(result, {"message": "Campo obbligatorio mancante o vuoto: cognome"})
+
+    #Caso input chiave mancante
+    def test_modificaAutotrasportatore_chiaveMancante(self):
+        self.mock_autotrasportatore_dao.__json__ = MagicMock(return_value = {
+            "id": 1,
+            "nome": "NomeTest",
+            "cognome": "CognomeTest",
+            "dataNascita": "1990-01-01",
+            "codiceFiscale": "RSSMRA85M01H501Z",
+            "email": "test@example.com",
+            "password": "testpassword",
+            "indirizzo": "Via Test, 123",
+            "qrCode_id": 1
+        })
+
+        result = AccountAutotrasportatoreController.modificaAutotrasportatore(self.mock_autotrasportatore_dao.__json__.return_value)
+
+        # Verificare che la funzione restituisca il risultato atteso
+        self.assertEqual(result, {"message": "Campo obbligatorio mancante o vuoto: azienda"})
+
+
+    #Caso input non valido:eccezione
     def test_modificaAutotrasportatore_exception(self):
 
-                # Simula un'eccezione nel dao
-                self.mock_autotrasportatore_dao.ottieniAutotrasportatorePerId.side_effect = Exception("Errore simulato")
-
                 # Chiamata al metodo di modifica con eccezione attesa
-                result = AccountAutotrasportatoreController.modificaAutotrasportatore({})
+                result = AccountAutotrasportatoreController.modificaAutotrasportatore(1)
 
-                # Verifica che il risultato contenga il messaggio di errore
                 expected_result = {"message": "Errore durante la modifica dell'account di un autotrasportatore"}
                 self.assertEqual(result, expected_result)
-
 
 
 if __name__ == '__main__':
