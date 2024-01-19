77304e8413b4e818a3d8226e666e20d8543eadd4
PaoloMurino <p.murino1@studenti.unisa.it>
2024-01-19 11:43:42 +0100

    Implementazione TestOperazioneServiceId.py

---
+++ backend/src/unitTests/TestOperazioneServiceId.py
@@ -0,0 +1,39 @@
+import unittest
+from datetime import datetime
+from unittest.mock import MagicMock
+from backend.src.dataManagement.services import OperazioneService
+
+class TestOperazioneService(unittest.TestCase):
+    def setUp(self):
+        # Creo un mock per l'OperazioneDAO; questo mock verrà usato al posto dell'istanza reale di 'OperazioneDAO' per simularne il comportamento
+        self.operazione_dao_mock = MagicMock()
+
+    def testOttieniOperazionePerId(self):
+        # Configuro il mock per restituire un'operazione
+
+        # Configuro il comportamento del mock dicendogli che quando il metodo __json__ viene chiamato, dovrebbe restituire un determinato valore
+        self.operazione_dao_mock.__json__ = MagicMock(return_value={
+            "id": 1,
+            "dataOperazione": datetime(2023, 1, 1).date(),
+            "tipo": "Consegna",
+            "descrizione": "Consegna merci elettroniche",
+            "puntoDestinazione": "Magazzino Centrale",
+            "stato": "In corso / Issue aperta",
+            "autotrasportatore_id": 1,
+            "operatoreIngresso_id": 11,
+            "operatoreMagazzino_id": 7,
+            "percorso_id": 1,
+            "veicolo_id": 1
+        })
+
+        # Configuro il mock del 'OperazioneDAO' in modo che quando viene chiamato il metodo, restituisca l'oggetto 'operazione_mock', simulando il comportamento della classe 'OperazioneDAO' durante il test
+        self.operazione_dao_mock.ottieniOperazionePerId.return_value = self.operazione_dao_mock
+
+        # Chiama il metodo da testare
+        risultato = OperazioneService.ottieniOperazionePerId(1)
+
+        # Verifica che il risultato sia quello atteso
+        self.assertEqual(risultato, self.operazione_dao_mock.__json__.return_value)
+
+if __name__ == '__main__':
+    unittest.main()
