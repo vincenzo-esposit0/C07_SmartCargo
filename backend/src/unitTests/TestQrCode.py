aea179fdd37266eec852811d19e7ccc111ccaa30
PaoloMurino <p.murino1@studenti.unisa.it>
2024-01-19 11:52:31 +0100

    Implementazione TestQrCode.py

--- backend/src/unitTests/TestQrCode.py
+++ backend/src/unitTests/TestQrCode.py
@@ -1,6 +1,9 @@
 import unittest
+from datetime import datetime
 from unittest.mock import  MagicMock
-from src.dataManagement.services import VeicoloService
+
+from backend.src.dataManagement.services import QrCodeService
+
 
 class TestQrCode(unittest.TestCase):
     def setUp(self):
@@ -9,13 +12,13 @@ class TestQrCode(unittest.TestCase):
        # Configuro il mock per restituire un'operazione
 
             # Configuro il comportamento del mock dicendogli che quando il metodo __json__ viene chiamato, dovrebbe restituire un determinato valore
-            self.qrcode_dao_mock.__json__ = MagicMock(return_value={{"id": 1, "isValido":1,"dataCreazione":"2023-01-01"}})
+            self.qrcode_dao_mock.__json__ = MagicMock(return_value={"id": 1, "isValido":0,"dataCreazione":datetime(2023, 1, 1).date()})
 
             # Configuro il mock del 'OperazioneDAO' in modo che quando viene chiamato il metodo, restituisca l'oggetto 'operazione_mock', simulando il comportamento della classe 'OperazioneDAO' durante il test
             self.qrcode_dao_mock.ottieni_qrCode_per_id.return_value = self.qrcode_dao_mock
 
             # Chiama il metodo da testare
-            risultato = OperazioneService.QrCodeService.ottieniQRCodePerId(1)
+            risultato = QrCodeService.ottieniQRCodePerId(1)
 
             # Verifica che il risultato sia quello atteso
             self.assertEqual(risultato, self.qrcode_dao_mock.__json__.return_value)
